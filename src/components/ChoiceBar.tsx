9068173738b4013cb3b391e2e2843e7a31d5d8d3
Falko Schumann <falko.schumann@muspellheim.de>
2020-06-02 16:17:52 +0200

    4.4 Styling der Komponente

--- src/components/ChoiceBar.tsx
+++ src/components/ChoiceBar.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function ChoiceBar() {
   return (
-    <div>
-      <div>JavaScript</div>
-      <div>123</div>
+    <div className="ChoiceBar">
+      <div className="Progress" style={{ width: '37%' }}>
+        <div className="ChoiceBarTitle">JavaScript</div>
+      </div>
+      <div className="ChoiceBarBadge">123</div>
     </div>
   );
 }
